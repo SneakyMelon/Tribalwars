eb6edf9fc0c18da1e442e6dc44013aa2464162c6
Allan Davidson <allan_m_e_davidson@hotmail.com>
2016-09-07 16:52:43 +0100

    Spelling / Grammar

--- scripts/script.js
+++ scripts/script.js
@@ -17,5 +17,5 @@ if (typeof window.$ === "function" && typeof window.$.getScript "function") {
   UI.SuccessMessage("Loading scripts... Please wait...", 2000);
   window.$.getScript("https://raw.githubusercontent.com/SneakyMelon/Tribalwars/master/scripts/farming/Non%20Randomised%20Farming/Tribalwars%20Farming%20Script.js");
 } else {
-  UI.ErrorMessage("Can not access getScript Method. Please submit an error <a href='https://gitub.com/SneakyMelon/Tribalwars/issues/new'>bug report</a>"", 5000);
+  UI.ErrorMessage("Can not access getScript Method. Please submit a new <a href='https://gitub.com/SneakyMelon/Tribalwars/issues/new'>bug report</a>", 5000);
 }
