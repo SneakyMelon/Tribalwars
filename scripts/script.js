daf214004e1244678646a80fc4227628751af053
Allan Davidson <allan_m_e_davidson@hotmail.com>
2016-09-07 17:14:22 +0100

    Spelling / Grammar

--- scripts/script.js
+++ scripts/script.js
@@ -13,9 +13,9 @@ var units = {
   	heavy : 0,
 };
 
-if (typeof window.$ === "function" && typeof window.$.getScript "function") {
-  UI.SuccessMessage("Loading scripts... Please wait...", 2000);
-  window.$.getScript("https://raw.githubusercontent.com/SneakyMelon/Tribalwars/master/scripts/farming/Non%20Randomised%20Farming/Tribalwars%20Farming%20Script.js");
+if (typeof window.$ === "function" && typeof window.$.getScript === "function") {
+  UI.SuccessMessage("Loading scripts... Please wait...", 4000);
+  window.$.getScript("https://sneakymelon.github.io/Tribalwars/scripts/farming/Non%20Randomised%20Farming/Tribalwars%20Farming%20Script.js");
 } else {
   UI.ErrorMessage("Can not access getScript Method. Please submit a new <a href='https://gitub.com/SneakyMelon/Tribalwars/issues/new'>bug report</a>", 5000);
 }
